f779632ff7a3c9036020b59978f4270890bfb512
Admin <Admin@LAPTOP-UQA792V7>
2023-10-24 20:21:05 +0700

    trang chu

--- WebsiteBanHang/Controllers/ProductController.cs
+++ WebsiteBanHang/Controllers/ProductController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanHang.Context;
+using WebsiteBanHang.Models;
 
 namespace WebsiteBanHang.Controllers
 {
     public class ProductController : Controller
     {
+        WebBanHangEntities obj = new WebBanHangEntities();
         // GET: Product
-        public ActionResult Index()
+        public ActionResult Detail(int id)
         {
-            return View();
+            var objProduct = obj.Products.Where(n => n.Id == id).FirstOrDefault();
+            return View(objProduct);
         }
     }
 }
